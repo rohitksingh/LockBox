408563965f93ddf41b93fee31c4f562d4e4a7b6c
Rohit <r4rohit002@gmail.com>
2021-03-18 22:59:28 -0700

    Created barebones for Login and SignupFragment

---
+++ app/src/main/java/com/rohitksingh/lockbox/CredentialActivity.java
@@ -0,0 +1,36 @@
+package com.rohitksingh.lockbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class CredentialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credential);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.credentialFragmentHolder, getFragment(), "CREDENTIAL_HOLDER")
+                .commit();
+
+    }
+
+    public Fragment getFragment(){
+
+        if(ifAlreadyLoggedIn()){
+            return LoginFragment.getInstance();
+        }else{
+            return SignUpFragment.getInstance();
+        }
+    }
+
+
+    private boolean ifAlreadyLoggedIn(){
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/rohitksingh/lockbox/SignUpFragment.java
@@ -0,0 +1,23 @@
+package com.rohitksingh.lockbox;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SignUpFragment extends Fragment {
+
+    public static SignUpFragment getInstance(){
+        return new SignUpFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.fragment_signup, parent, false);
+        return view;
+
+    }
+}
